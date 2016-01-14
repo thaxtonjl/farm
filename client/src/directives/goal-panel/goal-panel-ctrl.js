6c76071ce7ae037c3deb2873b009faa044f317ee
Josh Thaxton <thaxtonjl@gmail.com>
2016-01-13 19:08:20 -0500

    cap the money goal's width

--- client/src/directives/goal-panel/goal-panel-ctrl.js
+++ client/src/directives/goal-panel/goal-panel-ctrl.js
@@ -5,11 +5,19 @@
         .module('farmApp')
         .controller('GoalPanelCtrl', GoalPanelCtrl);
 
-    function GoalPanelCtrl() {
+    function GoalPanelCtrl($scope) {
 
         var vm = this;
 
-        this.moneyGoal = 100;
+        // Properties
+        vm.moneyGoal = 100;
+
+        // Methods
+        vm.percentageOfMoneyGoal = percentageOfMoneyGoal;
+
+        function percentageOfMoneyGoal() {
+            return (Math.min($scope.live.money, vm.moneyGoal) * 100 / vm.moneyGoal).toFixed(4);
+        }
 
     }
 
